8ccb714291497a86188509143d3e3e5eeda5bdf1
Maite Nigro <86933780+Maite2003@users.noreply.github.com>
2025-11-24 23:17:58 -0300

    prueba query alternativa

--- lib/data/internaciones.ts
+++ lib/data/internaciones.ts
@@ -7,44 +7,62 @@ interface InternacionResult {
 }
 
 export async function crearInternacion(data: crearInternacionProps) {
-  
-  try {
-    const resultadoTransaccion = await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
-      
-      const internacionCreada: InternacionResult[] = await tx.$queryRaw<InternacionResult[]>`
-        INSERT INTO INTERNACION (fecha_hora_inicio, nro_matricula, nro_dni)
-        VALUES (
-            NOW(), 
-            ${data.matriculaMedico},
-            ${data.dniPaciente} 
-        )
-        RETURNING id_internacion;
-      `;
+  await prisma.$executeRawUnsafe(`
+    BEGIN;
 
-      if (internacionCreada.length === 0 || !internacionCreada[0].id_internacion) {
-        throw new Error("No se pudo obtener el ID de la internación recién creada.");
-      }
-      
-      const newInternacionId = internacionCreada[0].id_internacion;
-      await tx.$executeRaw`
-        INSERT INTO INTERNACION_CAMA (fecha_hora_asignacion, id_internacion, id_habitacion, id_cama)
-        VALUES (
-            NOW(),
-            ${newInternacionId},
-            ${data.habitacionId},
-            ${data.camaId}
-        );
-      `;
-      
-      return newInternacionId;
-      
-    }, {});
+    -- 1. Crear internación
+    INSERT INTO INTERNACION (fecha_hora_inicio, nro_matricula, nro_dni)
+    VALUES (NOW(), ${data.matriculaMedico}, ${data.dniPaciente})
+    RETURNING id_internacion;
 
-    return resultadoTransaccion;
+    -- 2. Asignar cama inicial
+    INSERT INTO INTERNACION_CAMA (fecha_hora_asignacion, id_internacion, id_habitacion, id_cama)
+    VALUES (
+        NOW(),
+        currval('internacion_id_internacion_seq'),  -- Última internación creada
+        ${data.habitacionId},
+        ${data.camaId}
+    );
+
+COMMIT;
+    `);
+  // try {
+  //   const resultadoTransaccion = await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+      
+  //     const internacionCreada: InternacionResult[] = await tx.$queryRaw<InternacionResult[]>`
+  //       INSERT INTO INTERNACION (fecha_hora_inicio, nro_matricula, nro_dni)
+  //       VALUES (
+  //           NOW(), 
+  //           ${data.matriculaMedico},
+  //           ${data.dniPaciente} 
+  //       )
+  //       RETURNING id_internacion;
+  //     `;
+
+  //     if (internacionCreada.length === 0 || !internacionCreada[0].id_internacion) {
+  //       throw new Error("No se pudo obtener el ID de la internación recién creada.");
+  //     }
+      
+  //     const newInternacionId = internacionCreada[0].id_internacion;
+  //     await tx.$executeRaw`
+  //       INSERT INTO INTERNACION_CAMA (fecha_hora_asignacion, id_internacion, id_habitacion, id_cama)
+  //       VALUES (
+  //           NOW(),
+  //           ${newInternacionId},
+  //           ${data.habitacionId},
+  //           ${data.camaId}
+  //       );
+  //     `;
+      
+  //     return newInternacionId;
+      
+  //   }, {});
+
+  //   return resultadoTransaccion;
     
-  } catch (error) {
-    throw error;
-  }
+  // } catch (error) {
+  //   throw error;
+  // }
 }
 
 export async function getTodasLasInternaciones() {
